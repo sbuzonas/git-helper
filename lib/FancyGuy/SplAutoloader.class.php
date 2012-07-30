588fabf20b3f14cb56d94e94eee6acffed0cea9f
Steve Buzonas <steve@slbmeh.com>
2012-07-30 03:01:26 -0400

    fixed require error from hard coded directory due to refactor

--- lib/FancyGuy/SplAutoloader.class.php
+++ lib/FancyGuy/SplAutoloader.class.php
@@ -33,7 +33,7 @@
 
 namespace FancyGuy;
 
-require_once 'Exceptions' . DIRECTORY_SEPARATOR . 'ClassNotFoundException.class.php';
+require_once '../GitHelper/Exceptions/ClassNotFoundException.class.php';
 
 /**
  * Description of SplAutoloader
